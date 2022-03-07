a01286371615fc9702150a004d3af8c474ee5861
glebuben <90417655+glebuben@users.noreply.github.com>
2022-03-07 15:03:28 +0300

    Update ex02.hpp

--- ex02.hpp
+++ ex02.hpp
@@ -1,6 +1,6 @@
 //change the below code
 // the function below now only works with integers, change it to work with any type
-// also fill out the body
+// also fill out the body//
 template<typename T>
 class Calculator {
 private:
